40400afab5f871bac286f93d7043abb4f0210880
冰冻大西瓜 <1003582810@qq.com>
2026-07-27 00:29:39 +0800

    🔧 移除本地搜索配置并优化代码块标题图标样式
    - 移除 `search` 导入并注释搜索配置项
    - 调整代码块标题图标尺寸，完善 `.vp-code-block-title` 和 `.vp-code-group` 图标样式

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitepress'
-import { viteConfig, markdownConfig, sidebar, search } from './configs'
+import { viteConfig, markdownConfig, sidebar } from './configs'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: 'docs',
@@ -20,7 +20,7 @@ export default defineConfig({
       { text: 'Go 编程圣典', link: '/GoGuide/' },
     ],
     sidebar,
-    search,
+    // search,
     outline: 'deep',
   },
   vite: viteConfig,
